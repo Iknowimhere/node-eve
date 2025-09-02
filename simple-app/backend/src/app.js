6b3b9e3d4c790f9007092d1b6397093fc1f0428e
umashankar <umashankarp33@gmail.com>
2025-09-02 18:50:25 +0530

    add left out routes

--- simple-app/backend/src/app.js
+++ simple-app/backend/src/app.js
@@ -44,35 +44,94 @@ const productSchema = new mongoose.Schema(
 ); //createdAt updatedAt
 
 //model
-let Product=mongoose.model("Product",productSchema)
+let Product = mongoose.model("Product", productSchema);
 
 //middleware--parses incoming json data
-app.use(express.json())
+app.use(express.json());
 
 //create product
-app.post("/products",async (req,res,next)=>{
-  let {productName,price,category}=req.body;
-    try {
-      if(!productName || !category){
-        res.status(400).json({message:"Please fill all fields"})
-        return;
-      }
-      let newProduct= await Product.create({
-        productName,
-        price,
-        category
-      })
-      if(!newProduct){
-        res.status(400).json({message:"Error creating product"})
-        return;
-      }
-      res.status(201).json(newProduct)
-    } catch (error) {
-      console.log("error in create product route",error);
-      res.status(500).json({message:"Internal server error"})
+app.post("/products", async (req, res, next) => {
+  let { productName, price, category } = req.body;
+  try {
+    if (!productName || !category) {
+      res.status(400).json({ message: "Please fill all fields" });
+      return;
     }
+    let newProduct = await Product.create({
+      productName,
+      price,
+      category,
+    });
+    if (!newProduct) {
+      res.status(400).json({ message: "Error creating product" });
+      return;
+    }
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log("error in create product route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/products", async (req, res, next) => {
+  try {
+    let products = await Product.find();
+    if (products?.length <= 0) {
+      res.status(200).json({ message: "No products found" });
+      return;
+    }
+    res.status(200).json(products);
+  } catch (error) {
+    console.log("error in get product route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+//dymanic url
+//route parametes--they are named url segments which can be captured
+//The captured values are populated in the req.params object
+app.get("/products/:id",async (req,res)=>{
+  try {
+      let {id}=req.params
+      let product=await Product.findById(id)
+      if (!product) {
+      res.status(404).json({ message: `No product with this ${id} found` });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.log("error in getting single product route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 })
 
 
+app.patch("/products/:id",async(req,res)=>{
+  try {
+    let {id}=req.params
+    let updatedProduct=await Product.findByIdAndUpdate(id,{...req.body})
+     if (!updatedProduct) {
+      res.status(404).json({ message: `Error updating product` });
+      return;
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log("error in updating single product route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
 
+app.delete("/products/:id",async (req,res)=>{
+  try {
+      let {id}=req.params
+      let product=await Product.findByIdAndDelete(id)
+      if (!product) {
+      res.status(404).json({ message: `No product with this ${id} found` });
+      return;
+    }
+    res.status(204).json();
+  } catch (error) {
+    console.log("error in deleting single product route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
 export default app;
